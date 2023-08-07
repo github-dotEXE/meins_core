c241aeecf771281257f7c0fec2cf66f9f87774a7
dotEXE <jm3001jonathanmayer@gmail.com>
2023-08-07 13:17:12 +0200

    changed

--- src/main/java/de/ender/core/floattext/CustomFloatText.java
+++ src/main/java/de/ender/core/floattext/CustomFloatText.java
@@ -10,8 +10,8 @@ public class CustomFloatText{
     private final Display.Billboard billboard;
     private final boolean shadowed;
     private FloatText floatText = null;
-    private final float yaw;
-    private final float pitch;
+    private float yaw;
+    private float pitch;
 
     public CustomFloatText(String text, NamespacedKey id, Display.Billboard billboard, boolean shadowed,float yaw,float pitch) {
         this.text = text;
@@ -22,6 +22,9 @@ public class CustomFloatText{
         this.pitch = pitch;
     }
     public FloatText spawn(Location location){
+        if(!(-180<yaw&&yaw<180)) yaw = location.getYaw();
+        if(!(-90<pitch&&pitch<90)) pitch = location.getPitch();
+
         if(isSpawned()) setLocation(location);
         else floatText = FloatTextManager.addFloatText(location,text,shadowed,billboard,yaw,pitch,id);
         return floatText;
